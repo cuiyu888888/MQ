9f5596d796f226f78c05e82e0b219bf818eaaada
cuiyu <cuiyu@wenxiangcn.com>
2019-11-15 18:29:30 +0800

    Revert "10"
    This reverts commit ff7e0456

---
+++ activeMQ_demo/src/main/java/com/atguigu/active/a1.java
@@ -0,0 +1,17 @@
+package com.atguigu.active;/**
+ * @Author 崔雨
+ * @Date 2019/11/15 18:29
+ * @Description: ${todo}
+ */
+
+/**
+ * @program: mq
+ *
+ * @description:
+ *
+ * @author: Mr.Wang
+ *
+ * @create: 2019-11-15 18:29
+ **/
+public class a1 {
+}
