f5ce2d14e0950e23e677ff72285d2f89e685f2bf
cuiyu <cuiyu@wenxiangcn.com>
2019-11-15 17:13:11 +0800

    6

---
+++ activeMQ_demo/src/main/java/com/atguigu/active/ad.java
@@ -0,0 +1,17 @@
+package com.atguigu.active;/**
+ * @Author 崔雨
+ * @Date 2019/11/15 17:12
+ * @Description: ${todo}
+ */
+
+/**
+ * @program: mq
+ *
+ * @description:
+ *
+ * @author: Mr.Wang
+ *
+ * @create: 2019-11-15 17:12
+ **/
+public class ad {
+}
