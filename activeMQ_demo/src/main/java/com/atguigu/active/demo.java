d568211e1b5b815340062fec6252f432027786aa
cuiyu <cuiyu@wenxiangcn.com>
2019-11-15 16:57:25 +0800

    s

---
+++ activeMQ_demo/src/main/java/com/atguigu/active/demo.java
@@ -0,0 +1,33 @@
+package com.atguigu.active;/**
+ * @Author 崔雨
+ * @Date 2019/11/15 16:35
+ * @Description: ${todo}
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @program: mq
+ *
+ * @description:
+ *
+ * @author: Mr.Wang
+ *
+ * @create: 2019-11-15 16:35
+ **/
+public class demo {
+
+    public static void main(String[] args) {
+
+        List<String> s = new ArrayList<>();
+        s.add("a");
+        Collectors.c
+        for (String ss : s){
+            System.out.println(ss);
+            s.add("b");
+        }
+
+    }
+}
